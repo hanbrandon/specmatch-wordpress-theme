8b5292dc20a5d8ea3d64f94e30f4162b6b835dbc
Sang Hyun Han <hanbrandon@hotmail.com>
2026-07-30 11:07:50 -0700

    Add AdSense loader to theme head

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9481966710896505" crossorigin="anonymous"></script>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
